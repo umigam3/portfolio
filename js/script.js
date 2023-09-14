4153651f88c329e4c8796110b71a24dd8afe2fcd
Gerard Doncel <gerarddoncel41@gmail.com>
2023-09-14 18:18:25 +0200

    Added index reactive scroll

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function isElementInViewport (el) {
+  var rect = el.getBoundingClientRect();
+  var vHeight = window.innerHeight || document.documentElement.clientHeight;
+  console.log('vHeight: ' + vHeight); // Add this line
+
+  return (
+    rect.bottom >= 0 && // Element's bottom is at or above the top of the viewport
+    rect.top <= vHeight / 3 // Element's top is within the top half of the viewport
+  );
+}
+
+function handler() {
+  var visible = 0;
+  var ids = ['about','experience','education','projects'];
+  ids.forEach(function(id){
+    if(isElementInViewport($('#'+id)[0])) {
+        visible = id;
+    }
+  })
+  
+  if (visible) {
+  	$('.index-element').removeClass('selected');
+    $('#' + visible + '_').addClass('selected');
+  }
+};
+
+window.onload = function() {
+  const right = document.getElementsByClassName('right-section');
+  right[0].addEventListener('scroll', handler);
+}
